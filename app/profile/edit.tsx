939994df50d0d73e85b9f447603f91f452e5c071
Arthur Duarte <tuiduarte@gmail.com>
2025-05-01 16:23:33 -0400

    feat: add edit and follow buttons to profile header

---
+++ app/profile/edit.tsx
@@ -0,0 +1,11 @@
+import { ThemedView } from "@/components/ThemedView";
+import { View, Text } from "react-native";
+
+export default function EditProfile() {
+  return (
+    <ThemedView>
+      <Text>Edit Profile</Text>
+    </ThemedView>
+  );
+}
+
--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -1,32 +1,52 @@
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { View, StyleSheet, Text, Image, Button, TouchableOpacity } from 'react-native';
 import { Profile, Log } from '@/types/db';
+import { useAuth } from '@/contexts/AuthContext';
+import { router } from 'expo-router';
 
 interface ProfileHeaderProps {
   profile: Partial<Profile>
   logs: Log[]
 }
 export default function ProfileHeader({ profile, logs }: ProfileHeaderProps) {
+  const { profile: currentProfile } = useAuth();
+
   return (
       <View style={styles.header}>
-        <View>
-            <Image source={{ uri: profile?.image }} style={styles.avatar} />
-        </View>
-        <View style={styles.info}>
-            <Text style={styles.name}>{profile?.first_name} {profile?.last_name}</Text>
-            <View style={styles.stats}>
-                <View>
-                    <Text>Meals</Text>
-                    <Text style={styles.statValue}>{logs.length}</Text>
-                </View>
-                <View>
-                    <Text>Followers</Text>
-                    <Text style={styles.statValue}>10</Text>
-                </View>
-                <View>
-                    <Text>Following</Text>
-                    <Text style={styles.statValue}>10</Text>
-                </View>
+        <View style={styles.headerTop}>
+          <View>
+              <Image source={{ uri: profile?.image }} style={styles.avatar} />
+          </View>
+          <View style={styles.info}>
+            <View style={styles.nameContainer}>
+              <Text style={styles.name}>{profile?.first_name} {profile?.last_name}</Text>
+              <Text style={styles.username}>@{profile?.username}</Text>
             </View>
+              <View style={styles.stats}>
+                  <View>
+                      <Text>Meals</Text>
+                      <Text style={styles.statValue}>{logs.length}</Text>
+                  </View>
+                  <View>
+                      <Text>Followers</Text>
+                      <Text style={styles.statValue}>10</Text>
+                  </View>
+                  <View>
+                      <Text>Following</Text>
+                      <Text style={styles.statValue}>10</Text>
+                  </View>
+              </View>
+          </View>
+        </View>
+        <View style={styles.headerBottom}>
+            {currentProfile?.id === profile?.id ? (
+              <TouchableOpacity style={styles.followButton} onPress={() => router.push('/profile/edit')}>
+                <Text style={styles.followButtonText}>Edit Profile</Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity style={styles.followButton}>
+                <Text style={styles.followButtonText}>Follow</Text>
+              </TouchableOpacity>
+            )}
         </View>
       </View>
   );
@@ -34,15 +54,46 @@ export default function ProfileHeader({ profile, logs }: ProfileHeaderProps) {
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     padding: 16,
     alignItems: 'center',
     gap: 16,
+  },
+  headerTop: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  headerBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+    width: '100%',
+  },
+  followButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#793206',
+    color: '#fff',
+    borderRadius: 12,
+    padding: 10,
+    paddingHorizontal: 20,
+  },
+  followButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
   avatar: {
     width: 100,
     height: 100,
     borderRadius: 50,
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
   },
   info: {
     flex: 1,
@@ -50,6 +101,10 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#793206',
+  },
+  username: {
+    fontSize: 16,
     color: '#793206',
   },
   stats: {
